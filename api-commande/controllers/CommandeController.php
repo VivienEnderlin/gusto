d99abca7fe3d57974a81ad9569642d13926d85b8
VivienEnderlin <djiomounandavivienenderlin@gmail.com>
2026-06-08 19:57:41 +0100

    ninetheen steady commit

--- api-commande/controllers/CommandeController.php
+++ api-commande/controllers/CommandeController.php
@@ -123,7 +123,7 @@ class CommandeController {
 
         // Vérification table ouverte
         $table = $this->commande->ShowTableActive(
-            $data['nom'],
+            $nom,
             $id_etablissement
         );
 
