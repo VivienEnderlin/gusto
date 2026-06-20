cf313633172373f8d7375d7690d6eb73a3a976cf
VivienEnderlin <djiomounandavivienenderlin@gmail.com>
2026-06-20 07:41:57 +0100

    version stable 56

--- api-commande/controllers/QrCodeController.php
+++ api-commande/controllers/QrCodeController.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__ . '/../models/QrCode.php';
 require_once __DIR__ . '/../core/Middleware.php';
-require_once __DIR__ . '/../utils/phpqrcode/qrlib.php';
+
+if (!class_exists('QRcode')) {
+    require_once __DIR__ . '/../utils/phpqrcode/qrlib.php';
+}
 
 class QrCodeController {
 
