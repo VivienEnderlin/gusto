07477592175accbe85ccdb97c176f01ea0c7085d
VivienEnderlin <djiomounandavivienenderlin@gmail.com>
2026-08-30 21:16:13 +0100

    finalisation 23

--- api-commande/controllers/EtablissementController.php
+++ api-commande/controllers/EtablissementController.php
@@ -10,7 +10,7 @@ class EtablissementController {
     public function __construct() {
         Middleware::checkAuth();
         $this->etablissement = new Etablissement();
-        error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+        error_reporting(E_ALL);
     }
 
     // =========================
