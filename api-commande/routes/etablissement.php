69730a51e3afce81fd7a1ebf643badb88d0195e3
VivienEnderlin <djiomounandavivienenderlin@gmail.com>
2026-08-30 21:20:23 +0100

    finalisation 24

--- api-commande/routes/etablissement.php
+++ api-commande/routes/etablissement.php
@@ -1,8 +1,4 @@
 <?php
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 require_once __DIR__ . '/../core/Cors.php';
 require_once __DIR__ . '/../controllers/EtablissementController.php';
 require_once __DIR__ . '/../core/Middleware.php';
