e7d272bcc0a36603a7e894c17d5ae6ecc6219a50
VivienEnderlin <djiomounandavivienenderlin@gmail.com>
2026-08-30 19:30:39 +0100

    fnalisation 13

--- api-commande/config/upload.php
+++ api-commande/config/upload.php
@@ -1,5 +1,11 @@
 <?php
 
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+header('Content-Type: application/json; charset=utf-8');
+
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 // ============================================================
